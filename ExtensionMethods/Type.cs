eb1ab1e8a2851ac98a19eaaa00ee51c57afda858
Traber <traber@hotmail.es>
2025-08-20 19:44:04 +0100

    Add SchemaDefinition/Column class to support generic database column data typing Update SchemaDefinition/Column to add support to ColumnDataType enum Version bump

---
+++ ExtensionMethods/Type.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Unleasharp.DB.Base.SchemaDefinition;
+
+namespace Unleasharp.DB.Base.ExtensionMethods;
+public static class TypeExtensions {
+    public static ColumnDataType GetColumnType(this Type type) {
+        // unwrap Nullable<T>
+        Type underlyingType = Nullable.GetUnderlyingType(type) ?? type;
+
+        return underlyingType switch {
+            Type t when t == typeof(bool)      => ColumnDataType.Boolean,
+            Type t when t == typeof(short)     => ColumnDataType.Int16,
+            Type t when t == typeof(int)       => ColumnDataType.Int32,
+            Type t when t == typeof(long)      => ColumnDataType.Int64,
+            Type t when t == typeof(ushort)    => ColumnDataType.UInt16,
+            Type t when t == typeof(uint)      => ColumnDataType.UInt32,
+            Type t when t == typeof(ulong)     => ColumnDataType.UInt64,
+            Type t when t == typeof(float)     => ColumnDataType.Float,
+            Type t when t == typeof(double)    => ColumnDataType.Double,
+            Type t when t == typeof(decimal)   => ColumnDataType.Decimal,
+            Type t when t == typeof(string)    => ColumnDataType.Varchar,
+            Type t when t == typeof(DateTime)  => ColumnDataType.DateTime,
+            Type t when t == typeof(TimeSpan)  => ColumnDataType.Time,
+            Type t when t == typeof(Guid)      => ColumnDataType.Guid,
+            Type t when t == typeof(byte[])    => ColumnDataType.Binary,
+            //Type t when t == typeof(object)    => ColumnType.Text,
+
+            _ => throw new NotSupportedException(
+                     $"No DbColumnType mapping defined for CLR type {underlyingType.FullName}")
+        };
+    }
+
+    public static bool IsSigned(this Type type) {
+        // unwrap Nullable<T>
+        Type underlyingType = Nullable.GetUnderlyingType(type) ?? type;
+
+        return underlyingType switch {
+			Type t when t == typeof(short)     => true,
+			Type t when t == typeof(int)       => true,
+            Type t when t == typeof(long)      => true,
+            Type t when t == typeof(ushort)    => false,
+            Type t when t == typeof(uint)      => false,
+            Type t when t == typeof(ulong)     => false,
+        };
+
+        // If value is not handled, return TRUE by default to avoid "UNSIGNED" keyword
+        return true;
+    }
+}
--- SchemaDefinition/Column.cs
+++ SchemaDefinition/Column.cs
@@ -9,19 +9,24 @@ namespace Unleasharp.DB.Base.SchemaDefinition;
 
 [AttributeUsage(AttributeTargets.Property)]
 public class Column : NamedStructure {
-    public string  DataType      { get; }
-    public int     Length        { get; set; }
-    public int     Precision     { get; set; }
-    public bool    Unsigned      { get; set; } = false;
-    public bool    NotNull       { get; set; } = false;
-    public bool    AutoIncrement { get; set; } = false;
-    public bool    Unique        { get; set; } = false;
-    public bool    PrimaryKey    { get; set; } = false;
-    public string? Default       { get; set; }
-    public string? Comment       { get; set; }
+    public ColumnDataType DataType       { get; set; }
+    public string         DataTypeString { get; set; }
+    public int            Length         { get; set; }
+    public int            Precision      { get; set; }
+    public bool           Unsigned       { get; set; } = false;
+    public bool           NotNull        { get; set; } = false;
+    public bool           AutoIncrement  { get; set; } = false;
+    public bool           Unique         { get; set; } = false;
+    public bool           PrimaryKey     { get; set; } = false;
+    public string?        Default        { get; set; }
+    public string?        Comment        { get; set; }
 
-    public Column(string name, string dataType) : base(name) {
-        DataType = dataType;
+    public Column(string name, ColumnDataType dataType) : base(name) {
+		DataType       = dataType;
+    }
+
+    public Column(string name, string dataType)         : base(name) {
+        DataTypeString = dataType;
     }
 }
 
@@ -76,4 +81,30 @@ public class ForeignKey : NamedStructure {
         ReferencedTable  = referencedTable;
         ReferencedColumn = referencedColumn;
     }
+}
+
+public enum ColumnDataType {
+	Boolean,
+	Int,
+	Int16,
+	Int32,
+	Int64,
+	UInt,
+	UInt16,
+	UInt32,
+	UInt64,
+	Decimal,
+	Float,
+	Double,
+	Text,
+	Char,
+	Varchar,
+	Date,
+	DateTime,
+	Time,
+	Timestamp,
+	Binary,
+	Guid,
+	Json,
+	Xml
 }
