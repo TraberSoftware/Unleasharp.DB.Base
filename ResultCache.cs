a1f8665ecbf8e5f1040f2307edf363720b214f49
Traber <traber@hotmail.es>
2025-09-17 10:44:19 +0100

    Add ResultCache Add ResultCacheRow

---
+++ ResultCache.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unleasharp.DB.Base;
+public static class ResultCache {
+    private static MemoryCache __Cache { get; } = new MemoryCache(new MemoryCacheOptions {
+        ExpirationScanFrequency = TimeSpan.FromSeconds(30)
+    });
+
+    public static bool Set(int hashCode, ResultCacheRow row) {
+        if (row == null) {
+            return false;
+        }
+
+        return __Cache.Set(
+            hashCode, 
+            row, 
+            new MemoryCacheEntryOptions()
+                .SetSlidingExpiration (TimeSpan.FromMinutes(5)) // resets after each access
+                .SetAbsoluteExpiration(TimeSpan.FromMinutes(10)) // max total lifetime
+        ) != null;
+    }
+
+    public static ResultCacheRow Get(int hashCode) {
+        if (__Cache.TryGetValue(hashCode, out ResultCacheRow cached)) {
+            return cached;
+        }
+
+        return null;
+    }
+}
---
+++ ResultCacheRow.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Unleasharp.DB.Base.ExtensionMethods;
+using Unleasharp.DB.Base.QueryBuilding;
+using Unleasharp.DB.Base.SchemaDefinition;
+
+namespace Unleasharp.DB.Base;
+
+public class ResultCacheRow {
+    public Table                      Table;
+    public FieldSelector              KeyColumn;
+    public string                     KeyColumnName;
+    public Dictionary<string, object> Data;
+
+    public ResultCacheRow() { }
+
+    public ResultCacheRow(object row) {
+        Table    = row.GetType().GetCustomAttribute<Table>();
+        Data     = row.ToDynamicDictionary();
+
+        foreach (PropertyInfo property in row.GetType().GetProperties()) {
+            Column column = property.GetCustomAttribute<Column>();
+            if (column != null && column.PrimaryKey) {
+                KeyColumn     = new FieldSelector(Table.Name, column.Name);
+                KeyColumnName = column.Name;
+                break;
+            }
+        }
+    }
+}
